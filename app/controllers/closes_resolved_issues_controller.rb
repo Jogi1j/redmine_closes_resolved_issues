8fb2213f3ad1322b7680a1018dd4bda6fe0eb8d8
Jogi1j <100200@seznam.cz>
2015-03-10 07:27:05 +0100

    [ADD] documentation

--- app/controllers/closes_resolved_issues_controller.rb
+++ app/controllers/closes_resolved_issues_controller.rb
@@ -7,6 +7,8 @@ class ClosesResolvedIssuesController < ApplicationController
     @issues = Issue.all
   end
 
+  # status_id = 3 (is resolved) and status_id = 5 (is closed)
+  # if you delete ", :updated_on => Time.now", update time will not be updated
   def self.close
     Issue.where(:status_id => 3).where("updated_on < ?",
       Time.now - Setting.plugin_redmine_closes_resolved_issues['number_of_days'].to_i.days).
