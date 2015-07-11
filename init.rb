4688e908298819f8725d00d770fe3d5e2b620a8a
Kim Pepper <kim@pepper.id.au>
2015-07-12 09:46:41 +1000

    Fixes typo

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ require 'rufus/scheduler'
 Redmine::Plugin.register :redmine_closes_resolved_issues do
   name 'Redmine Closes Resolved Issues plugin'
   author 'Jogi1j'
-  description 'This is a plugin for Redmin, which closes resolved issues after x days.'
+  description 'This is a plugin for Redmine, which closes resolved issues after x days.'
   version '1.0.2'
   url 'http://www.redmine.org/plugins/redmine_closes_resolved_issues'
   author_url 'https://github.com/Jogi1j/redmine_closes_resolved_issues'
